c1f2b358e31e3f01fc7b7cfcbc8666aa77232283
farzandeali27335 <farzandeali27335@gmail.com>
2025-04-26 23:52:10 +0530

    Implemented project managment features like updation and deletion options

---
+++ app/src/main/java/com/example/freelanceworktracker/PorfileActivity.kt
@@ -0,0 +1,13 @@
+package com.example.freelanceworktracker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PorfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
